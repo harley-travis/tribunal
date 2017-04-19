91977c458f9dc0371dbd00fcda3a15cb9fab5ebd
harley-travis <harley.travis2@gmail.com>
2017-04-19 10:04:21 -0600

    updated the headsroll.js. also updated the main nav and some CSS. included files to the request-a-demo.php

--- page-templates/request-a-demo.php
+++ page-templates/request-a-demo.php
@@ -5,9 +5,18 @@
 ?>
 <?php get_template_part('/includes/header.php'); ?>
 
+	<div class="container-fluid pink-yellow">
+		<?php include('../includes/page-header.php'); ?>
+
+		<div class="container banner">
+			<div class="banner-wrapper">
+				<h1>The World's Most Powerful Brand, Quality, &amp; Safety Compliance Solution</h1>
+				
+			</div><!-- banner-wrapper -->
+		</div><!-- banner -->
+	</div><!-- container-fluid -->
 
 
 
 
-
-<?php get_template_part('/includes/footer.php'); ?>
+<?php get_template_part('/includes/footer.php'); ?>p
