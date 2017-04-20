3d3f8dabd7fc432a1e0ba85504bbfcd63b2865d9
Riley Hein <Riley.Hein@rizepoint.com>
2017-04-20 09:51:47 -0600

    updated demo page 4/19

--- page-templates/request-a-demo.php
+++ page-templates/request-a-demo.php
@@ -10,11 +10,60 @@
 
 		<div class="container banner">
 			<div class="banner-wrapper">
-				<h1>The World's Most Powerful Brand, Quality, &amp; Safety Compliance Solution</h1>
+			<h2>Request A Demo</h2>
+				<h3>The World's Most Powerful Brand, Quality, &amp; Safety Compliance Solution</h3>
 				
 			</div><!-- banner-wrapper -->
 		</div><!-- banner -->
 	</div><!-- container-fluid -->
+
+<div class="row">
+	<div class="col-md-6 col-sm-12 col-xs-12" request-video>
+			<h1>Why Choose RizePoint?</h1>
+				<p>The Worlds Most Powerful Brand Quality &amp; Safety Compliance Solution</p>
+		<img src="#">Video
+		
+	</div><!--request-video-->
+	
+	<div class="col-md-6 col-sm-12 col-xs-12" request-form>
+		<h3>Request A Free</h3>
+			<h1>Demo</h1>
+		
+	</div><!--request-form-->
+	
+	
+</div><!--row-->
+  
+<div class="row">
+	<div class="request-icons">
+		<div class="col-md-4 col-sm-6 col-xs-12">
+		
+		</div>
+		<div class="col-md-4 col-sm-6 col-xs-12">
+		
+		</div>
+		<div class="col-md-4 col-sm-6 col-xs-12">
+		
+		</div>
+		
+	</div><!--request-icons-->
+</div><!--row-->
+
+<div class="container-fluid">
+	<div class="request-brands-wrapper">
+	<div class="row">
+		<div class="request-brands">
+				<p>The Brands Who Trust RizePoint</p>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+	
+		</div>
+	
+		</div><!--request-brands-->
+	</div><!--row-->
+<button></button>
+
+	</div><!--request-brands-wrapper-->
+</div><!--container-fluid-->
 
 
 
