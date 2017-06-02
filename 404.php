db425c6e2444716152a2bddc0220fe7cdf8e55fe
hein-riley <Riley.Hein@rizepoint.com>
2017-06-02 14:04:56 -0600

    updated 404

--- 404.php
+++ 404.php
@@ -13,10 +13,10 @@ get_header(); ?>
 				<div class="row">
 					<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 error-mes">
 						<h1>404</h1>
-						<h3>You've made a huge mistake. <br> Try your URL again. <br> Click <a href="home.php">here</a> for the best summer ever.</h3>
+						<h3>You've made a huge mistake. <br> Try your URL again. <br> Click <a href="index.php">here</a> for the best summer ever.</h3>
 					</div>
 					<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 error-img">
-						<img src="<?php echo ROOT; ?>assets/img/gif/burn-ban-2.gif">
+						<img src="<?php echo ROOT; ?>assets/img/gif/burn-ban-2.gif" alt="Burning Stand">
 					</div>
 				</div><!--row-->
 			</div><!-- banner-wrapper -->
