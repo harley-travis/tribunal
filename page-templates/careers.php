c8792cd69313b9a4084a93d4b9f5ed27fbc953ff
hein-riley <Riley.Hein@rizepoint.com>
2017-06-08 14:08:39 -0600

    riley updates

--- page-templates/careers.php
+++ page-templates/careers.php
@@ -1,78 +1,142 @@
-<?php 
+<?php
 /**
  * Template Name: Careers
-*/
+ */
 get_header(); ?>
 
-
-	<div class="container-fluid pink-yellow">
-		<?php get_template_part('includes/page-header'); ?>
-
-<div class="container-banner">
-	<div class="banner-wrapper">
-		<h2>Careers</h2>
-			<h3>The World's Most Powerful Brand, Quality, &amp; Safety Compliance Solution</h3>
-				
+	
+<div class="container-fluid pink-yellow">
+	<?php include  __DIR__ . "/../includes/page-header.php"; ?>
+	<div class="container banner wrap push">
+		<div class="banner-wrapper">
+			<h1>Careers</h1>
+			<div class="banner-btns">
+				<button type="button" class="btn btn-lg btn-width btn-inverse-white orange-text" data-toggle="modal" data-target="#banner-video">Watch Video</button> <a href="#" class="btn btn-lg btn-width btn-inverse-white">Free Demo</a>
+			</div><!-- banner-btns -->
 		</div><!-- banner-wrapper -->
 	</div><!-- banner -->
 </div><!-- container-fluid -->
+
+<!-- banner video -->
+<div class="modal fade" id="banner-video" tabindex="-1" role="dialog" aria-labelledby="banner-videoLabel">
+	<div class="large-modal modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="banner-videoLabel">See How RizePoint Can Help The Food Industry</h4>
+			</div>
+			<div class="modal-body">
+				<script src="https://fast.wistia.com/embed/medias/in3fdjpcii.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_in3fdjpcii videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>
+			</div><!-- modal-body -->
+			<div class="modal-footer">
+				<button type="button" class="btn btn-green btn-medium">Request A Demo</button>
+			</div>
+		</div><!-- modal-content -->
+	</div><!-- modal-dialog -->
+</div><!-- modal -->
 	
+
 <div class="content-wrapper">
 
-<div class="careers-why">
-	<div class="row">
-	<div class="careers-why-title">
-		<h1>Why Work At RizePoint</h1>
+<div class="container-fluid">
+<div class="container careers-why">
+
+	
+	<div class="careers-why-title orangeTwo-text">
+		<h1>Why Work At RizePoint?</h1>
 	</div><!--careers-why-title-->
+	
+	
 		<div class="col-md-6 col-sm-12 col-xs-12 careers-why-text">
 			<ul>
 				<li>Culture that values work-life balance</li>
 				<li>Broad selection of medical, dental, and vision benefits</li>
 				<li>Excellent 401k</li>
 				<li>Fully stocked breakroom</li>
-			</ul>
-		</div><!--careers-why-text-->
-		
-		<div class="col-md-6 col-sm-12 col-xs-12 careers-why-text">
-			<ul>
 				<li>Onsite gym, daycare, cafeteria</li>
 				<li>open door executive meetings</li>
 			</ul>
 		</div><!--careers-why-text-->
-	</div><!--row-->
-</div><!--careers-why-->
-
-
-<div class="careers-values">
-	<div class="row">
-	<div class="careers-values-title">
-		<h2>Values</h2>
-	</div><!--careers-values-title-->
-		<div class="col-md-3 col-sm-12 col-xs-12 careers-values-text">
-			<ul>
-				<li><h2>Triumph</h2></li>
-				<li><h2>Empower</h2></li>
-				<li><h2>Unite</h2></li>
-				<li><h2>Share</h2></li>
-				<li><h2>Enjoy</h2></li>
-			</ul>
-		</div><!--careers-values-text-->
 		
-		<div class="col-md-9 col-sm-12 col-xs-12 careers-values-text">
-			<ul>
-				<li>Play to win</li>
-				<li>Create opportunity</li>
-				<li>Come together</li>
-				<li>Right ideas</li>
-				<li>Learn, grow and enjoy</li>
-			</ul>
-		</div><!--careers-values-text-->
-	</div><!--row-->
-</div><!--careers-values-->
+		<div class="col-md-6 col-sm-12 col-xs-12 careers-why-video">
+			<div class="careers-vid">
+				<script src="https://fast.wistia.com/embed/medias/ovpzlqhpdm.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.0% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_ovpzlqhpdm videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>
+			</div><!--careers-vid-->
+			
+		</div><!--careers-why-text-->
+
+	
+</div><!--careers-why-->
+</div><!--container-fluid-->
 
 
-<div class="careers-culture">
-	<div class="row">
+<div class="container-fluid">
+	<div class="container values-wrapper orangeTwo-bg">
+		
+
+
+				<div class="careers-values white-text">
+					
+					<div class="careers-values-title">
+						<h2>Values</h2>
+						</div><!--careers-values-title-->
+						
+						<div class="col-md-4 col-sm-12 col-xs-12 values-img-wrapper">
+						
+					<ul>
+						<li>
+							<img src="<?php echo ROOT; ?>assets\img\infographics\rocketboy.png">
+								
+								</li>
+								<li>
+									<img src="<?php echo ROOT; ?>assets\img\infographics\goblet.png">
+								
+								</li>
+								
+					</ul>
+						</div><!--values-img-wrapper-->
+						
+							<div class="col-md-4 col-sm-12 col-xs-12 values-img-wrapper">
+						
+					<ul>
+								<li>
+									<img src="<?php echo ROOT; ?>assets\img\infographics\meeting.png">
+								
+								</li>
+								<li>
+									<img src="<?php echo ROOT; ?>assets\img\infographics\party.png">
+								
+						</li>
+					</ul>
+						</div><!--values-img-wrapper-->
+					
+					<div class="col-md-4 col-sm-12 col-xs-12 careers-values-text">
+							
+							<ul>
+								<li><span class="left-ls">Triumph</span> <span class="right-ls">Play to win</span></li>
+								<li><span class="left-ls">Empower</span> <span class="right-ls">Create opportunity</span></li>
+								<li><span class="left-ls">Unite</span> <span class="right-ls">Come together</span></li>
+								<li><span class="left-ls">Share</span> <span class="right-ls">Right ideas</span></li>
+								<li><span class="left-ls">Enjoy</span> <span class="right-ls">Learn, grow and enjoy</span></li>
+								
+							</ul>
+						</div><!--careers-values-text-->
+						
+					
+					
+						
+				
+					
+				</div><!--careers-values-->
+		
+	</div><!--values-wrapper-->
+</div><!--container-fluid-->
+					
+<div class="container-fluid">
+<div class="container culture-wrapper">	
+
+<div class="careers-culture orangeTwo-text">
+	
 	<div class="careers-culture-title">
 		<h2>Culture</h2>
 	</div><!--careers-values-title-->
@@ -82,18 +146,19 @@ get_header(); ?>
 			</p>
 		</div><!--careers-values-text-->
 		
-		<div class="col-md-6 col-sm-12 col-xs-12 careers-culture-text">
+	<div class="col-md-2 col-sm-12 col-xs-12 careers-culture-text"></div>
+		<div class="col-md-4 col-sm-12 col-xs-12 careers-culture-text">
 			<ul>
 				<li>Clear directions that guide growth</li>
 				<li>Transparency through things like open door meetings</li>
 				<li>Focus on developing our individual capabilities</li>
+				<li>Focus on what matters</li>
+				<li>Deliver value to customer</li>
 			</ul>
 		</div><!--careers-values-text-->
 		
-		<div class="col-md-6 col-sm-12 col-xs-12 careers-culture-text">
+		<div class="col-md-4 col-sm-12 col-xs-12 careers-culture-text">
 			<ul>
-				<li>Focus on what matters</li>
-				<li>Deliver value to customer</li>
 				<li>Did we mention that customers kind of matter</li>
 				<li>Innovative and collaborative</li>
 				<li>Think differently about design</li>
@@ -101,52 +166,55 @@ get_header(); ?>
 				<li>Give back through Rize and Shine program</li>
 			</ul>
 		</div><!--careers-values-text-->
-	</div><!--row-->
+	<div class="col-md-2 col-sm-12 col-xs-12 careers-culture"></div>
+	
 </div><!--careers-values-->
+</div>
+</div>
 
 
-<div class="careers-locations">
-	<div class="row">
-	<div class="careers-locations-title">
-		<h2>Locations</h2>
-	</div><!--careers-locations-title-->
-		<div class="col-md-6 col-sm-12 col-xs-12 careers-locations-text">
-			<img src="<?php echo ROOT; ?>\assets\img\icons\content-icon1.png"
-		</div><!--careers-locations-text-->
+
+<div class="container-fluid">
+	<div class=" container location-wrapper blue-bg white-text">
+				
+					<div class="locations-title">
+						<h2>Locations</h2>
+					</div>
+					<div class="container location-content">
+						<div class="col-md-6 col-sm12 col-xs-12 careers-locations-img">
+							<img src="<?php echo ROOT; ?>\assets\img\icons\content-icon1.png">
+						</div><!--careers-locations-text-->
+
+						<div class="col-md-6 col-sm-12 col-xs-12 careers-locations-text">
+							<ul>
+								<li><h2>Salt Lake City</h2></li>
+								<li><h3>2890 East Cottonwood Pkwy, Suite 250</h3></li>
+								<li><h3>Salt Lake City, UT 84121</h3></li>
+							</ul>
+								<br>
+								<br>
+							<ul>
+								<li><h2>St. George</h2></li>
+								<li><h3>2890 East Cottonwood Pkwy, Suite 250</h3></li>
+								<li><h3>Salt Lake City, UT 84121</h3></li>
+							</ul>
+						</div><!--careers-locations-text-->
+					</div>
+					
+				
 		
-		<div class="col-md-6 col-sm-12 col-xs-12 careers-locations-text">
-			<ul>
-				<li><h3>Salt Lake City</h3></li>
-				<li>2890 East Cottonwood Pkwy, Suite 250</li>
-				<li>Salt Lake City, UT 84121</li>
-			</ul>
-			
-			<ul>
-				<li><h3>St. George</h3></li>
-				<li>2890 East Cottonwood Pkwy, Suite 250</li>
-				<li>Salt Lake City, UT 84121</li>
-			</ul>
-		</div><!--careers-locations-text-->
-	</div><!--row-->
-</div><!--careers-locations-->
 
+	</div><!--location-wrapper-->
+</div><!--container-fluid-->
 
 <div class="careers-job">
-	<div class="row">
-	<div class="careers-job-title">
+	<div class="careers-job-title blue-text">
 		<h2>Job Listings</h2>
-	</div><!--careers-job-title-->
-		<div class="col-md-12 col-sm-12 col-xs-12 careers-job-text">
+	</div><!--carees-job-title-->
+		<div class="careers-job-text">
+			<iframe src="https://rizepoint.applicantpro.com/iframe" width="100%"  height="600px" frameborder="0"></iframe>
 		</div><!--careers-job-text-->
-	</div><!--row-->
-</div><!--careers-job-->
-
-
-
-
-
-
-
+	</div><!--careers-job-->
 </div><!--content-wrapper-->
 
 
