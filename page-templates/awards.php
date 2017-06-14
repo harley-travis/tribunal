7364f816c02dd381be9e8bcd9fc061830049bff1
hein-riley <Riley.Hein@rizepoint.com>
2017-06-14 11:15:09 -0600

    awards page

---
+++ page-templates/awards.php
@@ -0,0 +1,307 @@
+<?php
+/**
+ * Template Name: Awards
+ */
+get_header(); ?>
+
+<style>
+.client-box-pic {
+    width: 100%;
+    max-width: 300px;
+    height: 300px;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+</style>
+
+<div class="container-fluid purple-pink">
+	<?php include  __DIR__ . "/../includes/page-header.php"; ?>
+	<div class="container banner wrap push">
+		<div class="banner-wrapper">
+			<h1>Awards</h1>
+			<div class="banner-btns">
+				<button type="button" class="btn btn-lg btn-width btn-inverse-white orange-text" data-toggle="modal" data-target="#banner-video">Visit the Blog</button> <a href="#" class="btn btn-lg btn-width btn-inverse-white">Community Relations</a>
+			</div><!-- banner-btns -->
+		</div><!-- banner-wrapper -->
+	</div><!-- banner -->
+</div><!-- container-fluid -->
+
+
+
+
+<section class="container-fluid clients-loop">	
+	<div class="container clients-loop-wrapper">
+		
+
+
+					
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+					
+						<div class="award-wrapper">
+							<div class="award-sec-wrapper">
+								<div class="client-box-pic">
+									<img src="https://rizepoint.com/wp-content/uploads/2017/06/cxo.png" alt="" class="rize-img">
+								</div><!-- client-box-pic -->
+							</div>
+							
+							<div class="client-name">
+								<h4><a href="https://rizepoint.com/press-releases/john-knotwell-named-cxo/">John Knotwell Named CXO of the Year  </a></h4>
+							</div><!-- client-name -->
+							<p>Date: June 14, 2017</p>
+							
+						</div>
+						
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+					
+						<div class="award-wrapper">
+							<div class="award-sec-wrapper">
+								<div class="client-box-pic">
+									<img src="https://rizepoint.com/wp-content/uploads/2017/06/silver.png" alt="" class="rize-img">
+								</div><!-- client-box-pic -->
+							</div>
+							
+							<div class="client-name">
+								<h4><a href="https://rizepoint.com/press-releases/john-knotwell-named-cxo/">John Knotwell Named CXO of the Year  </a></h4>
+							</div><!-- client-name -->
+							<p>Date: June 14, 2017</p>
+							
+						</div>
+						
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/06/silver.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/whitney-mccarthy-communication-professional/">RizePoint's Whitney McCarthy Wins Stevie for Communications Professional of the Year  </a></h4>
+						</div><!-- client-name -->
+						<p>Date: June 7, 2017   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/06/silver.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/executive-of-the-year/">Frank Maylett Wins Executive of the Year Stevie Award  </a></h4>
+						</div><!-- client-name -->
+						<p>Date: May 23, 2017   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/06/silver.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/corporate-social-responsibility-award/">RizePoint Corporate Social Responsibility Program Wins Stevie Award  </a></h4>
+						</div><!-- client-name -->
+						<p>Date: May 31, 2017   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/06/hermes.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://enter.hermesawards.com/winners/#/gold/2017">RizePoint Corporate Social Responsibility Program Wins Gold Hermes Creative Award </a></h4>
+						</div><!-- client-name -->
+						<p>Date: April 30, 2017   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/CEO-of-the-Year.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/frank-maylett-ceo-of-the-year/">Frank Maylett named CEO of the year</a></h4>
+						</div><!-- client-name -->
+						<p>Date: March 16, 2017</p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/02/2017-stevie-sales-customer-service.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/rizepoint-wins-silver-2017-stevie-awards-sales-customer-service/">RizePoint Wins Silver In 2017 Stevie Award </a></h4>
+						</div><!-- client-name -->
+						<p>Date: February 28, 2017 </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/forty-forty-logo.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/jesse-dowdle-forty-under-40/">Jesse Dowdle Named To Utah Business' Forty Under 40 </a></h4>
+						</div><!-- client-name -->
+						<p>Date: February 8, 2017  </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/06/ava.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://enter.avaawards.com/entry/join-the-rizepoint-team/">RizePoint Wins Platinum Award for Recruiting Video </a></h4>
+						</div><!-- client-name -->
+						<p>Date: January 30, 2017   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/lil-samy.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/michelle-suzuki-marketer-year/">Michelle Suzuki named Marketer of the year </a></h4>
+						</div><!-- client-name -->
+						<p>Date: January 19, 2017  </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-3 col-sm-6 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2017/01/silicon_review_fast_50.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/50-fastest-growing-tech-companies/">50 Fastest Growing Tech Companies </a></h4>
+						</div><!-- client-name -->
+						<p>Date: January 11, 2017  </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/12/food-logistics-min.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/rizepoint-named-food-logistics-2016/">Food Logistics' 2016 FL100+ Top Software and Technology Providers</a></h4>
+						</div><!-- client-name -->
+						<p>Date: December 16, 2016 </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/11/top-places-to-work.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/rizepoint-2016-top-workplace/">Salt Lake Tribune Names RizePoint As A 2016 Top Workplace </a></h4>
+						</div><!-- client-name -->
+						<p>Date: November 22, 2016  </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/uma.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/marketer-of-the-year/">Michelle Suzuki Named Marketer of the Year by Utah Marketing Association </a></h4>
+						</div><!-- client-name -->
+						<p>Date: November 21, 2016   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/rev-gen.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/john-knotwell-wins-revgen-2016-award/">John Knotwell Wins RevGen 2016 Award </a></h4>
+						</div><!-- client-name -->
+						<p>Date: November 3, 2016  </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/20-in-their-20s.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/brenna-hill-one-of-utah-business-magazine/">Brenna Hill Named One of Utah Business Magazine 20 in Their 20's </a></h4>
+						</div><!-- client-name -->
+						<p>Date: October 18, 2016   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/10/2016-stevies.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/2016-stevie-awards-for-great-employers/">RizePoint Wins Silver in 2016 Stevie® Award for Great Employers</a></h4>
+						</div><!-- client-name -->
+						<p>Date: October 3, 2016   </p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/women_tech_council_logo-300x127.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/michelle-suzuki-named-finalist-women-tech-awards/">Michelle Suzuki Named Finalist for Women Tech Award</a></h4>
+						</div><!-- client-name -->
+						<p>Date: August 4, 2016</p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/original-image_from_clipboard.png20160615-9314-1as37nv-300x180.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/frank-maylett-named-2016-ceo-year-utah-technology-council-2/">Frank Maylett Named 2016 CEO of the Year by Utah Technology Council</a></h4>
+						</div><!-- client-name -->
+						<p>Date: April 14, 2016</p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/original-image_from_clipboard.png20160615-9314-1as37nv-300x180.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/frank-maylett-named-2016-ceo-year-utah-technology-council-2/">John Knotwell Awarded Legislator of the Year by Utah Technology Council</a></h4>
+						</div><!-- client-name -->
+						<p>Date: April 14, 2016</p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/original-image_from_clipboard.png20160615-9314-3yt417-300x300.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/steton-mobile-auditor-wins-gold-stevie-award/">RizePoint Mobile Auditor Wins Gold Stevie Award</a></h4>
+						</div><!-- client-name -->
+						<p>Date: March 8, 2016</p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-12 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/original-image_from_clipboard.png20160615-9314-3yt417-300x300.png" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="https://rizepoint.com/press-releases/steton-named-as-finalist-in-2016-stevie-awards-for-sales-customer-service/">RizePoint Named as Finalist in 2016 Stevie Awards</a></h4>
+						</div><!-- client-name -->
+						<p>Date: February 8, 2016</p>
+					</div><!-- client-box-wrapper -->
+					
+					<div class="col-md-6 col-sm-6 col-xs-12 awards-box-wrapper">
+						<div class="client-box-pic">
+							<img src="https://rizepoint.com/wp-content/uploads/2016/06/CIO-20-most-promising-300x150.jpg" alt="" class="rize-img">
+						</div><!-- client-box-pic -->
+						<div class="client-name">
+							<h4><a href="http://compliance.cioreview.com/vendors/most-promising-compliance-technology-solution-providers-2015.html">RizePoint Makes CIO 20 Most Promising Compliance Solution Providers 2015</a></h4>
+						</div><!-- client-name -->
+						<p>Date: October 23, 2015</p>
+					</div><!-- client-box-wrapper -->
+
+
+
+
+	
+	</div><!-- clients-loop-wrapper -->
+</section><!-- clients-loop -->
+
+
+
+
+
+<?php get_footer(); ?>
