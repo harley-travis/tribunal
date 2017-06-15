b06297f80018b58791ad8d2956bade4c1c4b956b
harley-travis <harley.travis2@gmail.com>
2017-06-14 18:19:14 -0600

    fixed some bugs

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 // Root Paths
-define("ROOT","http://landing.rizepoint.com/wp-content/themes/the-living-tribunal/");
+define("ROOT","http://landing.rizepoint.com/wp-content/themes/tribunal/");
+define("ROOT_URL","http://landing.rizepoint.com");
 
 ?>
